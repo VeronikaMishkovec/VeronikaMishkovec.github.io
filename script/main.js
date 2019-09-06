f4cd1779f0ce8c32fd28241451dcb574e5935377
Veronika Kireenko <veronikakireenko@gmail.com>
2019-09-06 13:11:50 +0300

    add test-anix and test-unl

--- script/main.js
+++ script/main.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+    $('.anix').mouseover(function() {
+        $(this).css({'border':'0.5em solid #a3bbc7'});
+        $('.anix__buttons').css({'display':'flex'});
+    });
+    $('.anix').mouseleave(function() {
+        $(this).css({'border':'0.5em solid #e6c69e'});
+        $('.anix__buttons').css({'display':'none'});
+    });
+    $('.anix__ghpages').click(function() {
+        window.location = 'https://veronikamishkovec.github.io/test_anixmedia/';
+    });
+    $('.anix__github').click(function() {
+        window.location = 'https://github.com/VeronikaMishkovec/test_anixmedia/tree/master';
+    });
+    /*------------------------------------------------------------------------- */
+    $('.doob').mouseover(function() {
+        $(this).css({'border':'0.5em solid #a3bbc7'});
+        $('.doob__buttons').css({'display':'flex'});
+    });
+    $('.doob').mouseleave(function() {
+        $(this).css({'border':'0.5em solid #e6c69e'});
+        $('.doob__buttons').css({'display':'none'});
+    });
+    $('.doob__ghpages').click(function() {
+        window.location = 'https://veronikamishkovec.github.io/doob/';
+    });
+    $('.doob__github').click(function() {
+        window.location = 'https://github.com/VeronikaMishkovec/doob';
+    });
+    /*-------------------------------------------------------------------------*/
     $('.hexal').mouseover(function() {
         $(this).css({'border':'0.5em solid #a3bbc7'});
         $('.hexal__buttons').css({'display':'flex'});
@@ -13,7 +43,7 @@ $(document).ready(function() {
     $('.hexal__github').click(function() {
         window.location = 'https://github.com/VeronikaMishkovec/test-hexal';
     });
-
+    /*-------------------------------------------------------------------------*/
     $('.auris').mouseover(function() {
         $(this).css({'border':'0.5em solid #a3bbc7'});
         $('.auris__buttons').css({'display':'flex'});
